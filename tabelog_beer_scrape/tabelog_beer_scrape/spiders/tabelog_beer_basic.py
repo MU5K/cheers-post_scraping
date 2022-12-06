acfbac2f5c0a5399f53193fc09d02c6628db6d81
MU5K <yuca.yamauchi@gmail.com>
2022-12-06 22:25:10 +0900

    大阪市北区の店舗一覧の取得まで完了

--- tabelog_beer_scrape/tabelog_beer_scrape/spiders/tabelog_beer_basic.py
+++ tabelog_beer_scrape/tabelog_beer_scrape/spiders/tabelog_beer_basic.py
@@ -3,8 +3,18 @@ import scrapy
 
 class TabelogBeerBasicSpider(scrapy.Spider):
     name = 'tabelog_beer_basic'
-    allowed_domains = ['tabelog.com/osaka/C27127/rstLst/?SrtT=rt']
-    start_urls = ['http://tabelog.com/osaka/C27127/rstLst/?SrtT=rt/']
+    allowed_domains = ['tabelog.com']
+    start_urls = ['https://tabelog.com/osaka/C27127/rstLst/?SrtT=rt']
 
     def parse(self, response):
-        pass
+        restaurants = response.xpath('//h3/a[@class="list-rst__rst-name-target cpy-rst-name js-ranking-num"]')
+
+        for restaurant in restaurants:
+            yield {
+                'name': restaurant.xpath('.//text()').get(),
+                'URL': restaurant.xpath('.//@href').get()
+            }
+
+        next_page = response.xpath('//a[@rel="next"]/@href').get()
+        if next_page:
+            yield response.follow(url=next_page, callback=self.parse)
