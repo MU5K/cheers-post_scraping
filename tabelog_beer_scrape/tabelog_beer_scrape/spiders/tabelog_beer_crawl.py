51735bd374692caf72e71776b57b00769495f580
MU5K <yuca.yamauchi@gmail.com>
2022-12-09 00:14:12 +0900

    食べログの取得完了

--- tabelog_beer_scrape/tabelog_beer_scrape/spiders/tabelog_beer_crawl.py
+++ tabelog_beer_scrape/tabelog_beer_scrape/spiders/tabelog_beer_crawl.py
@@ -14,31 +14,35 @@ class TabelogBeerCrawlSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(restrict_xpaths='//h3/a[@class="list-rst__rst-name-target cpy-rst-name js-ranking-num"]')),
         Rule(LinkExtractor(allow=('dtlmenu/drink/')), callback='parse_item', follow=False),
-        # Rule(LinkExtractor(restrict_xpaths='//a[@rel="next"]'))
+        Rule(LinkExtractor(restrict_xpaths='//a[@rel="next"]'))
     )
 
     def parse_item(self, response):
         logging.info(response.url)
 
         soup = BeautifulSoup(response.text, 'html.parser')
-        drink = []
-        for drink_menu in soup.find_all("div", class_="rstdtl-menu-lst__info-inner"):
-            drink_name = drink_menu.find("p", class_="rstdtl-menu-lst__menu-title").text
-            drink_price = drink_menu.find("p", class_="rstdtl-menu-lst__price")
+        drink_names = []
+        drink_prices = []
+
+        for drink_list in soup.find_all("div", class_="rstdtl-menu-lst__info-inner"):
+            drink_name = drink_list.find("p", class_="rstdtl-menu-lst__menu-title").text
+            drink_price = drink_list.find("p", class_="rstdtl-menu-lst__price")
 
             if drink_price is not None:
-                drink_price = drink_menu.find("p", class_="rstdtl-menu-lst__price").text
+                drink_price = drink_list.find("p", class_="rstdtl-menu-lst__price").text
             else:
                 drink_name
 
-            drink.append({"drink_name": drink_name, "drink_price": drink_price})
+            drink_names.append(drink_name)
+            drink_prices.append(drink_price)
 
         loader = ItemLoader(item=RestaurantItem(), response=response)
         loader.add_xpath('restaurant_name', '//div[@class="rstinfo-table__name-wrap"]/descendant::node()/text()')
         loader.add_xpath('restaurant_address', '//p[@class="rstinfo-table__address"]/descendant::node()/text()')
         loader.add_xpath('restaurant_tel', '//th[contains(text(),"予約・")]/following-sibling::node()/p/strong[@class="rstinfo-table__tel-num"]/text()')
         loader.add_xpath('restaurant_url', '//li[@class="rstdtl-navi__sublist-item is-selected"]/a/@href')
-        loader.add_value('drink_menu', drink)
+        loader.add_value('drink_name', drink_names)
+        loader.add_value('drink_price', drink_prices)
         # loader.add_xpath('drink_price', '//div[@class="rstdtl-menu-lst__info-inner"]/p[2]/text()')
 
         yield loader.load_item()
